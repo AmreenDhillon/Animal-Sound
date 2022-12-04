60335a05574d929bd6ffe4e76c0e71ef18c9ceee
AmreenDhillon <92673783+AmreenDhillon@users.noreply.github.com>
2022-12-04 14:21:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/sOj3PyzV0/model.json',modelReady);
+}
+function modelReady() {
+    classifier.classify(gotResult);
+}
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
